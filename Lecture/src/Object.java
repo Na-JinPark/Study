5071d24f2209d50ece4aec99c91c6622f32f27f6
Na-JinPark <dijk00@nate.com>
2022-12-07 18:02:58 +0900

    클래스(진행중)

---
+++ Lecture/src/Object.java
@@ -0,0 +1,27 @@
+// Java 프로그래밍 - 클래스와 객체_1
+
+// Car 클래스 - 객체변수, 메소드
+
+class Car {
+    String name;
+    String type;
+
+    public  void printCarInfo(){
+        System.out.println("name");
+    }
+}
+
+// Car2 클래스 - 생성자, this
+
+
+public class Object {
+
+    public static void main(String[] args) {
+//      Car 클래스 사용
+
+
+//      Car2 클래스 사용
+
+    }
+
+}
